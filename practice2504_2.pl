0f147427f72d12e8623e27309a53a279f1fd72ee
Jim <chengxuyuan9527@gmail.com>
2022-04-25 23:05:09 +1000

    more practice

--- practice2504_2.pl
+++ practice2504_2.pl
@@ -14,9 +14,45 @@
 
 # $time = localtime;
 # print("$time\n");
-$^I = "*.tmp";
-while(<>){
-  s/friday/Monday/ig;
-  s/saturday/Tuesday/ig;
-  print;	
+# $^I = "*.tmp";
+# while(<>){
+  # s/friday/Monday/ig;
+  # s/saturday/Tuesday/ig;
+  # print;	
+# }
+
+# for(1..10) {
+   # print("$_\n");	
+# }
+
+# @str="jim";
+# foreach $i(@str) {
+   # print("$i\n")
+# }
+
+# print("Hello world\n");
+# sleep(5);
+# print("haha");
+
+# chomp($num = <>);
+# 
+# print("negative number is $num\n") if $num <0;
+
+sub error() {
+   $msg = shift @_;
+   print("Error message: $msg\n");
 }
+
+sub valid(){
+      $input = shift @_;
+      if ($input =~ /^[0-9]+$/) {
+          return 1;     
+       } else {
+          return 0;  
+        }
+}
+
+print("Please enter a number: \n");
+chomp($input=<STDIN>);
+&error("Invalid Input") unless &valid($input);
+
